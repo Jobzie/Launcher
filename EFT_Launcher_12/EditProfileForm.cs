eee5dfecf95c6e0d8b8a302c18be245f5a1fbcb7
BALIST0N <BALIST0N@users.noreply.github.com>
2019-12-04 06:56:06 +0100

    gofuck yourself github

--- EFT_Launcher_12/EditProfileForm.cs
+++ EFT_Launcher_12/EditProfileForm.cs
@@ -1,48 +1,45 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.IO;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace EFT_Launcher_12
 {
     public partial class EditProfileForm : Form
     {
-        int id;
-        string serverFolder = "Y:/tarkov/EmuTarkov Server dev"; //delete this
         string profilePath;
         ProfileExtended profileToEdit;
         List<HideoutUpgradesArea> hideoutLevels;
 
         public EditProfileForm(int id)
         {
-            this.id = id;
-            this.profilePath = Path.Combine(this.serverFolder, "appdata/profiles/character_" + this.id + ".json"); //Program.profileFolder + "character_" + this.id
+            profilePath = Path.Combine(Globals.serverFolder, "appdata/profiles/" + id + "/character.json");
             hideoutLevels = new List<HideoutUpgradesArea>();
 
             #region hideoutlevel init
 
-            hideoutLevels.Add( new HideoutUpgradesArea(0, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(1, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(2, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(3, "Stash", 4) );
-            hideoutLevels.Add( new HideoutUpgradesArea(4, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(5, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(6, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(7, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(8, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(9, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(10, "Workbench", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(11, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(12, "", 1) );
-            hideoutLevels.Add( new HideoutUpgradesArea(13, "", 1) );
-            hideoutLevels.Add( new HideoutUpgradesArea(14, "", 1) );
-            hideoutLevels.Add( new HideoutUpgradesArea(15, "", 3) );
-            hideoutLevels.Add( new HideoutUpgradesArea(16, "", 1) );
-            hideoutLevels.Add( new HideoutUpgradesArea(17, "", 1) );
-            hideoutLevels.Add( new HideoutUpgradesArea(18, "", 1) );
-            hideoutLevels.Add( new HideoutUpgradesArea(19, "", 1) );
-            hideoutLevels.Add( new HideoutUpgradesArea(20, "Bitcoin Farm", 3) );
+            hideoutLevels.Add(new HideoutUpgradesArea(0, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(1, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(2, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(3, "Stash", 4));
+            hideoutLevels.Add(new HideoutUpgradesArea(4, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(5, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(6, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(7, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(8, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(9, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(10, "Workbench", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(11, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(12, "", 1));
+            hideoutLevels.Add(new HideoutUpgradesArea(13, "", 1));
+            hideoutLevels.Add(new HideoutUpgradesArea(14, "", 1));
+            hideoutLevels.Add(new HideoutUpgradesArea(15, "", 3));
+            hideoutLevels.Add(new HideoutUpgradesArea(16, "", 1));
+            hideoutLevels.Add(new HideoutUpgradesArea(17, "", 1));
+            hideoutLevels.Add(new HideoutUpgradesArea(18, "", 1));
+            hideoutLevels.Add(new HideoutUpgradesArea(19, "", 1));
+            hideoutLevels.Add(new HideoutUpgradesArea(20, "Bitcoin Farm", 3));
 
             #endregion
             InitializeComponent();
@@ -54,7 +51,7 @@ namespace EFT_Launcher_12
             {
                 using (StreamReader r = new StreamReader(profilePath))
                 {
-                    this.profileToEdit = JsonConvert.DeserializeObject<ProfileExtended>(r.ReadToEnd());
+                    profileToEdit = JsonConvert.DeserializeObject<ProfileExtended>(r.ReadToEnd());
                     SetInfo();
                 }
             }
@@ -64,13 +61,10 @@ namespace EFT_Launcher_12
                 this.Close();
             }
 
-            foreach(HideoutUpgradesArea h in hideoutLevels)
+            foreach (HideoutUpgradesArea h in hideoutLevels)
             {
-                this.hideoutAreaComboBox.Items.Add(h.areaName);
+                hideoutAreaComboBox.Items.Add(h.areaName);
             }
-
-            this.hideoutAreaComboBox.SelectedIndex = 0;
-
         }
 
         private void saveButton_Click(object sender, EventArgs e)
@@ -78,20 +72,22 @@ namespace EFT_Launcher_12
             profileToEdit.Info.Nickname = nicknameTextBox.Text;
             profileToEdit.Info.Side = sideselectorComboBox.SelectedItem.ToString();
             profileToEdit.Info.Experience = Convert.ToInt32(experienceBox.Value);
-            profileToEdit.Info.GameVersion = gameVersionCombo.Text;
 
-
-
+            using (StreamWriter file = File.CreateText(profilePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, profileToEdit);
+            }
         }
 
         private void SetInfo()
         {
-            this.Text += profileToEdit.Info.Nickname;
+            Text += profileToEdit.Info.Nickname;
 
-            this.nicknameTextBox.Text = profileToEdit.Info.Nickname;
-            this.sideselectorComboBox.SelectedItem = profileToEdit.Info.Side;
-            this.experienceBox.Value = profileToEdit.Info.Experience;
-            this.gameVersionCombo.SelectedItem = profileToEdit.Info.GameVersion;
+            nicknameTextBox.Text = profileToEdit.Info.Nickname;
+            sideselectorComboBox.SelectedItem = profileToEdit.Info.Side;
+            experienceBox.Value = profileToEdit.Info.Experience;
+            gameVersionCombo.SelectedItem = profileToEdit.Info.GameVersion;
 
             #region INIT SKILLS numericBoxes
             this.enduranceNumericBox.Value = GetSkillValue("Endurance");
@@ -114,26 +110,21 @@ namespace EFT_Launcher_12
             this.searchNumericBox.Value = GetSkillValue("Search");
             this.magdrillsNumericBox.Value = GetSkillValue("MagDrills");
             #endregion
-
         }
 
         private void SaveProfile()
         {
-            using (StreamWriter file = File.CreateText(profilePath))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, profileToEdit);
-            }
+            // intentionally empty
         }
 
         private decimal GetSkillValue(string skill)
         {
-            return this.profileToEdit.Skills.Common[profileToEdit.Skills.Common.FindIndex(x => x.Id.Equals(skill))].Progress;
+            return profileToEdit.Skills.Common[profileToEdit.Skills.Common.FindIndex(x => x.Id.Equals(skill))].Progress;
         }
 
         private void SetSkillValue(string skill, decimal newval)
         {
-            this.profileToEdit.Skills.Common[profileToEdit.Skills.Common.FindIndex(x => x.Id.Equals(skill))].Progress = newval;
+            profileToEdit.Skills.Common[profileToEdit.Skills.Common.FindIndex(x => x.Id.Equals(skill))].Progress = newval;
         }
 
         private void hideoutAreaComboBox_SelectedIndexChanged(object sender, EventArgs e)
@@ -144,26 +135,23 @@ namespace EFT_Launcher_12
 
         private void hideoutLevelNumeric_ValueChanged(object sender, EventArgs e)
         {
-            
             profileToEdit.Hideout.Areas.Find(x => x.type.Equals(this.hideoutAreaComboBox.SelectedIndex)).level = Convert.ToInt32(hideoutLevelNumeric.Value);
         }
-
-
 
         /// <summary>
         /// hideout upgrades level object
         /// </summary>
         internal class HideoutUpgradesArea
         {
-            public int areaType { get; set; }
-            public string areaName { get; set; }
-            public int levelMax { get; set; }
+            public int areaType;
+            public string areaName;
+            public int levelMax;
 
             public HideoutUpgradesArea(int a, string n, int u)
             {
-                this.areaType = a;
-                this.areaName = n;
-                this.levelMax = u;
+                areaType = a;
+                areaName = n;
+                levelMax = u;
             }
         }
     }
